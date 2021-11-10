43e88a2b9bce2cee4138a05f6754d019d1e11439
ddael <80047646+ddael@users.noreply.github.com>
2021-11-10 23:13:26 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,127 +1,89 @@
 #include <iostream>
-#include <ctime>
-#include <string>
+#include <time.h>
+#include <tuple>
+#include <bitset>
 
-using namespace std;
 
-unsigned long units(unsigned long& x, unsigned long& y) { //еденицы
-
-    string a = to_string(x);
-    string b = to_string(y);
-
-    int c = 0;
-    int c1 = 0;
-
-    for (int i = 0; i < 10; i++) {
-        if (a[i] == '1') {
-            c += 1;
-        }
-
-        if (b[i] == '1') {
-            c1 += 1;
-        }
-
+int log2(int n) {
+    int x = 0;
+    while (n > 0) {
+        n = n & (n-1);
+        ++x;
     }
-
-    if (c > c1) {
-        return x;
-    }
-
-    else if (c < c1) {
-        return y;
-    }
-
-    else return 5;
-
+    return x;
+}
+int get_rand() {
+    return rand() % 1024;
 }
 
-unsigned long sum(unsigned long& x, unsigned long& y) {
+void print_swap(int from, int tob) {
 
-    string a = to_string(x);
-    string b = to_string(y);
-    string c;
-
-    for (int i = 0; i < 10; i++) {
-        if ((a[i] == '1') || (b[i] == '1')) {
-            c += '1';
-        }
-        else c += '0';
-    }
-
-    return stol(c); // перевод строки в число
+}
+void print(int num, std::string label) {
+    std::bitset<10> b_num(num);
+    std::cout << label << ": " << b_num << " : " << log2(num) <<"\t|" << std::endl;
 }
 
+std::tuple<int, int> process(int a, int b) {
+    int d = a | b;
+    int d_units = log2(d);
+
+    print(d, "d");
+    if (d_units > log2(a)) {
+        print_swap(b, d);
+        a = d;
+    }
+    else if (d_units > log2(b)) {
+        print_swap(b, d);
+        b = d;
+    }
+
+    return std::make_tuple(a, b);
+}
 int main() {
-    srand(time(NULL));
-    unsigned long a = 1, b = 1, c = 1;
-    int number = 0;
-    int epochs;
-    cout << "Write number of epochs: " << endl;
+    using namespace std;
+    srand((unsigned)time(NULL) );
+
+    cout << "number of epochs: " << endl;
+    int epochs = 0;
     cin >> epochs;
-    for (int i = 0; i < 9; i++){
-        a *= 10;
-        a += rand() % 2;
-        b *= 10;
-        b += rand() % 2;
-        c *= 10;
-        c += rand() % 2;
+
+    int num = 0;
+    int a, b, c = 0;
+    a = get_rand();
+    b = get_rand();
+    c = get_rand();
+
+    string operation [3] = {"a + b", "a + c", "b + c"};
+
+    for (int i = 0; i < epochs; ++i) {
+        num = rand() % 3;
+        cout << i << ": " << num << " -> " << operation[num] <<"           |" << endl;
+        print(a, "a");
+        print(b, "b");
+        print(c, "c");
+        cout  << "\t\t\t|"<< endl;
+        switch(num) {
+            case 0: {
+                tie(a, b) = process(a, b);
+                break;
+            }
+            case 1: {
+                tie(a, c) = process(a, c);
+                break;
+            }
+            case 2: {
+                tie(b, c) = process(b, c);
+                break;
+            }
+        }
+        cout << "-------------------------" << endl;
     }
+    cout << "=========================" << endl;
+    print(a, "a");
+    print(b, "b");
+    print(c, "c");
+    cout << "=========================" << endl;
 
-    cout << "initial epoch"<<endl;
-    cout << "a --- " << a << endl << "b --- " << b << endl << "c --- " << c << endl;
-
-    for (int i = 1; i <= epochs; i++) {
-        cout << "________________" << endl;
-        number = rand() % 3;
-
-        if (number == 0) {
-            unsigned long z = units(a, b);
-            if (z == a && b < sum(a, b)){
-                b = sum(a, b);
-            }
-            else if (z == b && a < sum(a, b)){
-                a = sum(a, b);
-            }
-            else {
-                unsigned long tmp = sum(a, b);
-                a = tmp;
-                b = tmp;
-            }
-            cout<<" a + b,  "<<i<<"  epoch"<<endl;
-        }
-
-        if (number == 1) {
-            unsigned long z = units(b, c);
-            if (z == b && c < sum(b, c)){
-                c = sum(b, c);
-            }
-            else if (z == c && b < sum(b, c)){
-                b = sum(b, c);
-            }
-            else {
-                unsigned long tmp = sum(b, c);
-                b = tmp;
-                c = tmp;
-            }
-            cout<<" b + c,  "<<i<<"  epoch"<<endl;
-        }
-
-        if (number == 2) {
-            unsigned long z = units(a, c);
-            if (z == a && c < sum(a, c)){
-                c = sum(a, c);
-            }
-            else if (z == c && a < sum(a, c)){
-                a = sum(a, c);
-            }
-            else {
-                unsigned long tmp = sum(a, c);
-                a = tmp;
-                c = tmp;
-            }
-            cout<<" a + c,  "<<i<<"  epoch"<<endl;
-        }
-        cout << "a --- " << a << endl << "b --- " << b << endl << "c --- " << c << endl;
-    }
     return 0;
 }
